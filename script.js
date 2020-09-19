238b3f62ca8e678e9913ee9da5a79b800e2e93f9
JOHN DANQUAH-BOATENG <yjbdanquah@gmail.com>
2020-09-19 02:15:27 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+// register service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
+// selected elements
+const notesArea = document.querySelector('#notes-area');
+const title = document.querySelector('#title');
+const editor = document.querySelector('#editor');
+const submit = document.querySelector('#submit');
+let data = [];
+window.onload = () => {
+    data = localStorage.getItem('noteObj') ? JSON.parse(localStorage.getItem('noteObj')) : [];
+    displayNotes(data);
+}
+
+submit.addEventListener('click', async function (e) {
+   const date =  new Date().toLocaleString();
+   console.log('dsat:', date);
+   const st = { 
+       date: date,
+       title: title.value,
+       editor: editor.value
+    }  
+    data.push(st);
+    await localStorage.setItem("noteObj", JSON.stringify(data));
+    title.value = '';
+    editor.value ='';
+    displayNotes(data);
+    console.log('st: ',st);  
+})
+
+ async function displayNotes (data){
+   data = data.sort(compareValues('date', 'desc'));
+
+    const noteItem = data.map(setNotes);
+    notesArea.innerHTML = noteItem;    
+}
+
+const setNotes = note => {
+    const output = `
+            <div class="col-lg-3 col-sm-12">
+                <div class="notes shadow">
+                    <h4 aria-label="topic">${note.title}</h4>
+                    <p aria-label="notes">${ note.editor}
+                    </p>
+                </div>
+            </div>
+    `
+    return output;
+}
+
+const checkObj = (obj) => {
+    for (var i in obj) return true;
+    return false;
+}
+
+function compareValues(key, order = 'asc') {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      // property doesn't exist on either object
+      return 0;
+    }
+
+    const varA = (typeof a[key] === 'string')
+      ? a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === 'string')
+      ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return (
+      (order === 'desc') ? (comparison * -1) : comparison
+    );
+  };
+}
---
+++ sw.js
@@ -0,0 +1,91 @@
+let filesToCache = resourceToCache();
+
+const staticCacheName = 'yourNOTES-v1';
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(response => {
+          // Respond with custom 404 page
+          if (response.status === 404) {
+            // console.log('404 ', event.request.url, ' not founds');
+            return caches.match('/not-found.html');
+          }
+          return caches.open(staticCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          }).catch(er => {
+            console.log('not from network: ', er);
+          });
+        });
+
+      }).catch(error => {
+        console.log("Hey your app is offline", error);
+        //Respond with custom offline page
+        return caches.match('/offline.html');
+      })
+  );
+});
+
+
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+  
+  const cacheAllowlist = [staticCacheName];
+  
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+function resourceToCache() {
+  return [
+    '/',
+    'style.css',
+    'index.html',
+    'all.min.js',
+    'script.js',
+    'jquery.min.js',
+    'bootstrap.min.js',
+    'bootstrap.min.css',
+    'android-chrome-192x192.png',
+    'android-chrome-256x256.png',
+    'apple-touch-icon.png',
+    'browserconfig.xml',
+    'favicon.ico',
+    'favicon-16x16.png',
+    'favicon-32x32.png',
+    'mstile-150x150.png',
+    'safari-pinned-tab.svg',
+    'site.webmanifest'
+  ];
+}
